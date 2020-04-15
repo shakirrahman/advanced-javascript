f9cccd0b0c74b97ff5848f62a55d3cb37e7e8bcf
shakirrahman <shakir.anth@gmail.com>
2020-04-15 15:39:16 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,15 @@
+//falsy:
+// 0
+//""
+//undefined
+// null
+// NaN
+// truthy
+//"0" " "
+let name = false;
+console.log(name);
+if (name) {
+    console.log("condition is true");
+} else {
+    console.log("condition is false");
+}
