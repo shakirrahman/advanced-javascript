cd57857ed6747139bdd9518e6a22c71f1bd190a4
shakirrahman <shakir.anth@gmail.com>
2020-04-15 16:01:25 +0600

    null vs undefined

---
+++ null-undefined.js
@@ -0,0 +1,17 @@
+//let pakhi;
+// function add(num1, num2) {
+//     console.log(num1 + num2);
+//     return
+//}
+// function add(num1, num2){
+//     console.log(num1, num2);
+// }
+// const premik = {name:"smart dude", phone:25786};
+
+// console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2,3,4];
+console.log(ages[11]);
