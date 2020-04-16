3969fd9fdf65ab89d8ea69b21c3d449741a81b18
shakirrahman <shakir.anth@gmail.com>
2020-04-16 11:12:41 +0600

    learned many things

---
+++ map-filter.js
@@ -0,0 +1,29 @@
+const numbers = [3, 4, 56, 8, 6, 9];
+// const output = [];
+
+// for (let i = 0; i < numbers.length; i++) {
+//     const element = numbers[i];
+//     const result = element * element;
+//     output.push(result);
+// }
+
+// function square(element) {
+//     return element * element;
+// }
+
+// const square = element=> element * element;
+// const square = x => x*x;
+
+// const result = numbers.map(function(element){
+//     return element * element;
+// })
+
+// const result = numbers.map(x => x * x);
+
+// console.log(result);
+
+const bigger = numbers.filter(x => x>5);
+
+const isThere = numbers.find(x => x > 5);
+
+console.log(isThere);
